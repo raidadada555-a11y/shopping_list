f15b459f9ef0388758f5972e272af94c8915bd20
raidadada555-a11y <raidadada555@gmail.com>
2026-08-14 09:53:12 +0900

    shopping_lists table to completed_shopping_lists修正-githubで直接修正-

--- database/migrations/2026_08_06_223950_create_completed_shopping_lists_table.php
+++ database/migrations/2026_08_06_223950_create_completed_shopping_lists_table.php
@@ -6,18 +6,25 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
-        Schema::create('shopping_lists', function (Blueprint $table) { 
+        // ここを 'completed_shopping_lists' に変更
+        Schema::create('completed_shopping_lists', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id');
-            $table->string('name', 255);
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('name');
             $table->timestamps();
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
-        Schema::dropIfExists('shopping_lists'); 
+        Schema::dropIfExists('completed_shopping_lists');
     }
 };
