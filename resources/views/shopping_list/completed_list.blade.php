aa330364296796b75f8514709f2f84ba5cf19f9c
raidadada555-a11y <raidadada555@gmail.com>
2026-08-16 19:32:18 +0900

    completed shopping items 修正8/15-githubより直接修正-
    Replaced static completed shopping items with dynamic data from the completedShoppingLists collection and added pagination links.

--- resources/views/shopping_list/completed_list.blade.php
+++ resources/views/shopping_list/completed_list.blade.php
@@ -9,7 +9,7 @@
 
     <!-- 「買うもの」一覧に戻るリンク -->
     <p>
-        <a href="/shopping_list/list">「買うもの」一覧に戻る</a>
+        <a href="{{ route('front.list') }}">「買うもの」一覧に戻る</a>
     </p>
 
     <!-- 一覧テーブル -->
@@ -18,19 +18,17 @@
             <th style="border: 3px double black; padding: 5px 10px;">「買うもの」名</th>
             <th style="border: 3px double black; padding: 5px 10px;">購入日</th>
         </tr>
-        {{-- 後ほど購入済みデータをここにループで表示します --}}
-        <tr>
-            <td style="border: 3px double black; padding: 5px 10px;">ウズラの卵</td>
-            <td style="border: 3px double black; padding: 5px 10px;">2021/12/09</td>
-        </tr>
+        @foreach ($completedShoppingLists as $completedShoppingList)
+            <tr>
+                <td style="border: 3px double black; padding: 5px 10px;">{{ $completedShoppingList->name }}</td>
+                <td style="border: 3px double black; padding: 5px 10px;">{{ $completedShoppingList->created_at }}</td>
+            </tr>
+        @endforeach
     </table>
     
     <!-- ページネーション表示 -->
     <div style="margin-top: 10px;">
-        <p>現在 1 ページ目</p>
-        <div>
-            最初のページ / 前に戻る / 次に進む
-        </div>
+        {{ $completedShoppingLists->links() }}
     </div>
 
     <hr style="margin: 20px 0;">
