e8b316aedaba8cb23e6796ae73cf9f45bf55e740
raidadada555-a11y <raidadada555@gmail.com>
2026-08-16 19:54:22 +0900

    CompletedShoppingList.php修正8/15-githubより直接修正-

--- app/Models/CompletedShoppingList.php
+++ app/Models/CompletedShoppingList.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class CompletedShoppingList extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+    ];
 }
